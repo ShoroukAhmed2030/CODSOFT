fdfb6b8b6e967bb8d4d4fb054115af20e976d62f
Shorouk Ahmed <140117465+ShoroukAhmed2030@users.noreply.github.com>
2023-09-10 01:42:17 +0300

    Add files via upload

---
+++ Calc.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+double num1, num2, ans;
+char opr;
+
+double sum(double, double);
+double sub(double, double);
+double mul(double, double);
+double div(double, double);
+
+int main() {
+
+  while (true) {
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    cout << "Enter the operation (+, -, *, /): ";
+    cin >> opr;
+
+    
+    switch (opr) {
+      case '+':
+        ans = sum(num1, num2); break;
+      case '-':
+        ans = sub(num1, num2); break;
+      case '*':
+      case 'x':
+      case 'X':
+        ans = mul(num1, num2); break;
+      case '/':
+        if (num2 == 0) {
+          cout << "ERROR: Can't divide by zero.";
+        } else {
+          ans = div(num1, num2); 
+        } break;
+      default:
+        cout << "Wrong operation."; break;
+    }
+    cout << num1 << opr << num2 << " = " << ans<<endl;
+  }
+
+  return 0;
+}
+
+double sum(double num1, double num2) {
+  return num1 + num2; }
+
+double sub(double num1, double num2) {
+  return num1 - num2; }
+
+double mul(double num1, double num2) {
+  return num1 * num2; }
+
+double div(double num1, double num2) {
+  return num1 / num2; }
